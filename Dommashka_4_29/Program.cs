e113ece585f6fdcff19cf544b1407f6fd6f55482
Vlad <vlad_zaycev_1984@mail.ru>
2022-10-29 00:21:28 +0300

    не полчилась

---
+++ Dommashka_4_29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая создает массив из некоторого количества элеентов
+// и выводит их на экран с помощью функции.
+
+Console.Clear();
+
+/*Console.WriteLine("Введите первый элемент массива: ");
+int[i] intArray = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второй элемент массива: ");
+int[i] intArray = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третий элемент массива: ");
+int[i] intArray = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(intArray);*/
+//int[i] intArray = new int[3]
+
+
+int i = 5;
+int[] A = new int [i];
+
+if(i > 0)
+{
+A[i] = Convert.ToInt32(Console.ReadLine());
+i=i+1;
+}
