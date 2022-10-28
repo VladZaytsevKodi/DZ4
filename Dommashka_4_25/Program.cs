a933bb84e1467514b91b635ad7c5a849e59e7681
Vlad <vlad_zaycev_1984@mail.ru>
2022-10-28 23:11:44 +0300

    4_25

---
+++ Dommashka_4_25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите функцию (цикл), который принимает на вход 2 числа (А и В) и возводит число А
+// в натуральную степень В.
+
+Console.Clear();
+
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число В: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if(b>=0)
+{
+Console.WriteLine($"A^B= {Math.Pow(a, b)}");
+}
+
+if(b<0)
+{
+int bi = b * -1; 
+Console.WriteLine($"A^B= {Math.Pow(a, bi)}");
+}
---
+++ Dommashka_4_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int sum = 0;
+while (n > 0)
+{  
+   sum = sum + n % 10; 
+   n = n / 10;
+}
+
+while (n < 0)
+{  
+   sum = sum + n % 10; 
+   n = n / 10;
+}
+Console.WriteLine(sum);
+
+
